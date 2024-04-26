90a89bbdff88972aba161353b3849f651d0ba0e4
mohamedelkhoreby <mohgamal518@gmail.com>
2024-04-26 18:10:51 +0300

    diffrenet user

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -9,11 +10,13 @@ class ChatScreen extends StatefulWidget {
   ChatScreenState createState() => ChatScreenState();
 }
 
+late User signedIn; //this will give us the email
+
 class ChatScreenState extends State<ChatScreen> {
   final TextEditingController _massageController = TextEditingController();
   final _fireStore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
-  late User signedIn; //this will give us the email
+
   String? massageText; //this will give us massage
   @override
   void initState() {
@@ -67,30 +70,7 @@ class ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            StreamBuilder<QuerySnapshot>(
-              stream: _fireStore.collection("massages").snapshots(),
-              builder: (context, snapshot) {
-                List<Text> messageWidgets = [];
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                }
-                if (!snapshot.hasData) {
-                  return const Text('No data available');
-                }
-                final messages = snapshot.data?.docs;
-                for (var message in messages!) {
-                  final messageText = message.get('text');
-                  final messageSender = message.get('sender');
-                  final messageWidget = Text("$messageText + $messageSender");
-                  messageWidgets.add(messageWidget);
-                }
-                return Expanded(
-                  child: ListView(
-                     children: messageWidgets,
-                  ),
-                );
-              },
-            ),
+            StreamBuilderMessage(fireStore: _fireStore),
             Container(
               decoration: const BoxDecoration(
                 border: Border(
@@ -139,6 +119,89 @@ class ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class StreamBuilderMessage extends StatelessWidget {
+  const StreamBuilderMessage({
+    super.key,
+    required FirebaseFirestore fireStore,
+  }) : _fireStore = fireStore;
+
+  final FirebaseFirestore _fireStore;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: _fireStore.collection("massages").snapshots(),
+        builder: (context, snapshot) {
+          List<Widget> messageWidgets = [];
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+          if (!snapshot.hasData) {
+            return const Text('No data available');
+          }
+          final messages = snapshot.data?.docs;
+          for (var message in messages!) {
+            final messageText = message.get('text');
+            final messageSender = message.get('sender');
+            final currentSigninUser = signedIn.email;
+            if (currentSigninUser == messageSender) {}
+            final messageWidget = MessageLine(
+              isMe: currentSigninUser == messageSender,
+              text: messageText,
+              sender: messageSender,
+            );
+
+            messageWidgets.add(messageWidget);
+          }
+          return Expanded(
+              child: ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            children: messageWidgets,
+          ));
+        });
+  }
+}
+
+class MessageLine extends StatelessWidget {
+  const MessageLine({Key? key, this.sender, this.text, required this.isMe})
+      : super(key: key);
+  final String? sender;
+  final String? text;
+  final bool? isMe;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment:
+            isMe !? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(
+            "$sender",
+            style: const TextStyle(fontSize: 8, color: Colors.black45),
+          ),
+          Material(
+            elevation: 5,
+            borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30)),
+            color: isMe! ? Colors.blue[800] : Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Text(
+                '$text',
+                style: TextStyle(
+                    fontSize: 15, color: isMe! ? Colors.white : Colors.black),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
